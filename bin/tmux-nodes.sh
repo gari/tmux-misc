20e67eb3feba52a2f102f33d43b5d2d42fd185a1
Ryan Woodsmall <rwoodsmall@splicemachine.com>
2016-05-12 00:28:30 +0000

    reuse exisitng (possibly incorrect) sessions; notes on making this better behaved

--- bin/tmux-nodes.sh
+++ bin/tmux-nodes.sh
@@ -5,12 +5,31 @@ if [ ${#} -eq 0 ] ; then
   exit 1
 fi
 
+# XXX - run "bash -l" by default, use "getent" or something similar to get user's shell (or environment var)
+# XXX - runs "ssh ..." by default, might need options passed (or environment var)
+# XXX - probably want to pick a static, normal tmux session name, like "tmux-nodes" and use -A/-D to create/attach/detach; always use that as our "home" session
+# XXX - probably want to check for $TMUX environment var
+# XXX - pane names might be set to passed name/IP of SSH hosts(? probably not, given length, ., etc.)
+
+# use PID, date/timestamp and a random number for session and window name(s)
 sessname="${$}-$(date +%Y%m%d%H%M%S)-${RANDOM}"
-winname="${#}-nodes"
+winname="${$}-${#}-nodes"
+
+# split mode alternates between v and h
 splitmode="v"
+
+# counter for pane name
 n=1
 
-tmux new-session -d -s ${sessname} -n ${winname} '/bin/bash -l'
+tmux list-sessions >/dev/null 2>&1
+if [ $? -ne 0 ] ; then
+  # if we don't have a session, create one
+  tmux new-session -d -s ${sessname} -n ${winname} '/bin/bash -l'
+else
+  # otherwise just use the first session name
+  sessname="$(tmux list-sessions | head -1 | cut -f1 -d:)"
+  tmux new-window -d -n ${winname} '/bin/bash -l'
+fi
 tmux set-window-option -g automatic-rename off
 tmux set-window-option -g allow-rename off
 tmux select-window -t ${sessname}:${winname}
